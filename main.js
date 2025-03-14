aba1ab4a5689de4b1e2f9742b08c1d3d175e031a
vine <vin.vinum0@gmail.com>
2025-03-14 20:29:53 -0300

    javascript adicionado

---
+++ main.js
@@ -0,0 +1,5 @@
+// // Simulação de adição de produtos a cesta do site
+const botoesAdicionar = document.querySelectorAll('.adicionar');
+const listaPedido = document.getElementById('lista -pedido');
+const totalElemento = document.getElementById('total');
+let total = 0;
